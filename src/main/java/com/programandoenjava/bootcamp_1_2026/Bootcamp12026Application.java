ceb458d6ba2d04af3027d74a72914fa08c86a49d
Ricardo Romero Benitez <ricardoromebeni@gmail.com>
2026-02-06 11:32:24 +0100

    Initialize Spring Boot project with Maven setup and essential files.

---
+++ src/main/java/com/programandoenjava/bootcamp_1_2026/Bootcamp12026Application.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.bootcamp_1_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp12026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp12026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/programandoenjava/bootcamp_1_2026/Bootcamp12026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.bootcamp_1_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp12026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
