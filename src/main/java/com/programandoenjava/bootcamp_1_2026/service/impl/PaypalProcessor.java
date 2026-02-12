80d85fb5426fd4922a112f4101f4fbe26752c3d9
Pablo <pabloprietolopez94@outlook.es>
2026-02-12 22:39:41 +0100

    feat(semana-1): add payment processors and DI architecture

---
+++ src/main/java/com/programandoenjava/bootcamp_1_2026/service/impl/PaypalProcessor.java
@@ -0,0 +1,24 @@
+package com.programandoenjava.bootcamp_1_2026.service.impl;
+
+import com.programandoenjava.bootcamp_1_2026.service.PaymentProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("prod")
+@ConditionalOnProperty(name = "app.payment-provider", havingValue = "paypal" )
+public class PaypalProcessor implements PaymentProcessor {
+
+    private final Environment env;
+    public PaypalProcessor(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void process(double amount) {
+        System.out.println("Conectando a Paypal API mediante clave: " + env.getProperty("app.paypal-key"));
+        System.out.println("Pago procesado por la cantidad de : " + amount + "€");
+    }
+}
