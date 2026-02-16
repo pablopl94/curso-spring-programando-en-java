7cdf42a7a369cca20291fb53262213e7d3d846dd
Pablo <pabloprietolopez94@outlook.es>
2026-02-16 22:35:17 +0100

    feat: AuditablePaymentProcessor with lifecycle logging

--- src/main/java/com/programandoenjava/bootcamp_1_2026/service/impl/MockProcessor.java
+++ src/main/java/com/programandoenjava/bootcamp_1_2026/service/impl/MockProcessor.java
@@ -1,18 +1,17 @@
 package com.programandoenjava.bootcamp_1_2026.service.impl;
 
-import com.programandoenjava.bootcamp_1_2026.service.PaymentProcessor;
+import com.programandoenjava.bootcamp_1_2026.service.AuditablePaymentProcessor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("dev")
-public class MockProcessor implements PaymentProcessor{
+public class MockProcessor extends AuditablePaymentProcessor {
 
     @Override
     public void process(double amount) {
-
         System.out.println("Probando MockProcessor desde desarrollo ..." );
         System.out.println("Pago procesado por la cantidad de : " + amount + "€");
-
     }
+
 }
