5ae88b36f98be2dfcd3e969daa2183d017e7e9ef
Pablo <pabloprietolopez94@outlook.es>
2026-02-22 11:16:49 +0100

    fix: use setAttribute to avoid variable override in interceptor

--- src/main/java/com/programandoenjava/bootcamp_1_2026/interceptor/PerformanceInterceptor.java
+++ src/main/java/com/programandoenjava/bootcamp_1_2026/interceptor/PerformanceInterceptor.java
@@ -1,6 +1,5 @@
 package com.programandoenjava.bootcamp_1_2026.interceptor;
 
-import com.programandoenjava.bootcamp_1_2026.filter.TransactionFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.Nullable;
@@ -10,17 +9,24 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 public class PerformanceInterceptor implements HandlerInterceptor {
 
-    long requestStart = 0L;
-    private static final Logger log = LoggerFactory.getLogger(TransactionFilter.class);
+    private static final Logger log = LoggerFactory.getLogger(PerformanceInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        requestStart = System.currentTimeMillis();
+        // Con HttpServletRequest creamos un objeto por cada petición, así no se sobreescribe
+        // y las peticiones no comparten una variable que podría sobreescribirla si se lanzan a la vez de dos peticiones
+        request.setAttribute("startTime",System.currentTimeMillis());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        log.debug("Tiempo total de respuesta : {} ms", System.currentTimeMillis() - requestStart);
+        Long start = (Long) request.getAttribute("startTime");
+        if(start == null) {
+            log.warn("Error: No se ha ejecutado correctamente preHandle");
+            return;
+        }
+        Long totalTime = System.currentTimeMillis() - start;
+        log.debug("Tiempo total de respuesta : {} ms", totalTime);
     }
 }
