2d458b099449acdd32c6b95ae9cfe3247fe043a6
Pablo <pabloprietolopez94@outlook.es>
2026-03-22 20:33:26 +0100

    refactor: change to class

--- src/main/java/com/programandoenjava/bootcamp_1_2026/order/model/api/request/RequestOrderFilter.java
+++ src/main/java/com/programandoenjava/bootcamp_1_2026/order/model/api/request/RequestOrderFilter.java
@@ -1,11 +1,20 @@
 package com.programandoenjava.bootcamp_1_2026.order.model.api.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public record RequestOrderFilter(
-        LocalDateTime createdAtFrom,
-        LocalDateTime createdAtTo,
-        Double totalAmountMin,
-        Double totalAmountMax,
-        String productName
-) { }
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestOrderFilter {
+    private LocalDateTime createdAtFrom;
+    private LocalDateTime createdAtTo;
+    private Double totalAmountMin;
+    private Double totalAmountMax;
+    private String productName;
+}
