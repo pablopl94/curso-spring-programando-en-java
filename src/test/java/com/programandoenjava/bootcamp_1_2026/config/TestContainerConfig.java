edd3f05eb74887e0be9358c980c7e30b661e9c0a
Pablo <pabloprietolopez94@outlook.es>
2026-03-22 20:35:32 +0100

    feat: add abstract class testcontainer postgresql config for test

---
+++ src/test/java/com/programandoenjava/bootcamp_1_2026/config/TestContainerConfig.java
@@ -0,0 +1,22 @@
+package com.programandoenjava.bootcamp_1_2026.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class TestContainerConfig {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+}
