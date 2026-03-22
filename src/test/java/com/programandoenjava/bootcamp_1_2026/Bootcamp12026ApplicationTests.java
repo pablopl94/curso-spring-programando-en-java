1a42fd324eddd634e2e3dd987d711134c81c16e0
Pablo <pabloprietolopez94@outlook.es>
2026-03-22 20:59:48 +0100

    chore: extends TestContainerConfig to applicationtest

--- src/test/java/com/programandoenjava/bootcamp_1_2026/Bootcamp12026ApplicationTests.java
+++ src/test/java/com/programandoenjava/bootcamp_1_2026/Bootcamp12026ApplicationTests.java
@@ -1,13 +1,14 @@
 package com.programandoenjava.bootcamp_1_2026;
 
+import com.programandoenjava.bootcamp_1_2026.config.TestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Bootcamp12026ApplicationTests {
+class Bootcamp12026ApplicationTests extends TestContainerConfig {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
